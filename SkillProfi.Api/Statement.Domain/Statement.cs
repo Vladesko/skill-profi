9632e3a9d39979c2321833425428d2f4656b4037
Vladesko <borodich01@list.ru>
2023-05-29 11:26:53 +0300

    InitCreate

---
+++ SkillProfi.Api/Statement.Domain/Statement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statement.Domain
+{
+    public class Statement
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public State Statatus { get; set; }
+        public enum State
+        {
+            Check,
+            Approved,
+            Denied,
+        }
+        
+    }
+}
