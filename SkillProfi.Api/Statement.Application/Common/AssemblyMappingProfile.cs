02779153bf69207435a288028cf757fbdf0cd009
Vladesko <borodich01@list.ru>
2023-06-02 11:41:53 +0300

    Created base for Mapping

---
+++ SkillProfi.Api/Statement.Application/Common/AssemblyMappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Statements.Application.Interfaces;
+using System.Reflection;
+
+namespace Statements.Application.Common
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly)
+        {
+            ApplyMappingsFromAssembly(assembly);
+        }
+        /// <summary>
+        /// Scans the assembly and looks for any types that implement the IMapWith interface
+        /// </summary>
+        /// <param name="assembly"></param>
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes().Where(type => type.GetInterfaces().
+               Any(intr => intr.IsGenericType && intr.GetGenericTypeDefinition() == typeof(IMapWith<>))).
+               ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
---
+++ SkillProfi.Api/Statement.Application/Interfaces/IMapWith.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+
+namespace Statements.Application.Interfaces
+{
+    public interface IMapWith<T>
+    {
+        //Create configuration from type <T>
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
