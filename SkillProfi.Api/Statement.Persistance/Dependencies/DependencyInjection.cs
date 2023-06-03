a5ec3aaf43bc22129c75e767fefb40119dfee994
Vladesko <borodich01@list.ru>
2023-06-03 17:26:46 +0300

    Added Dependency

---
+++ SkillProfi.Api/Statement.Persistance/Dependencies/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Statements.Application.Interfaces;
+
+namespace Statements.Persistance.Dependencies
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// This method add context for database
+        /// </summary>
+        /// <returns></returns>
+        public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration["DbConnection"];
+            services.AddDbContext<StatementDbContext>(options => 
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<IStatementDbContext>(provider => provider.GetService<StatementDbContext>());
+            return services;
+        }
+    }
+}
